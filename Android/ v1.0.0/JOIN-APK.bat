b0a0728dab32eba3cdb4224c64f4f6cf86ce7153
Punitpritam788 <punitpritam788@gmail.com>
2026-09-21 01:28:47 +0530

    Add files via upload

---
+++ Android/ v1.0.0/JOIN-APK.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+copy /b "Sleepy-Bat-Android-v1.0.0.apk.001"+"Sleepy-Bat-Android-v1.0.0.apk.002"+"Sleepy-Bat-Android-v1.0.0.apk.003" "Sleepy-Bat-Android-v1.0.0.apk"
+echo.
+echo APK rebuilt as Sleepy-Bat-Android-v1.0.0.apk
+pause
